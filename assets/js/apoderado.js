c0ad4d778d70859e0760a0ac4f8618732a92ddf6
Pamelaacuna <pame.acuna@duocuc.cl>
2026-09-16 07:29:03 -0300

    avanzar modulo apoderado con hijos y aporte de saldo

---
+++ assets/js/apoderado.js
@@ -0,0 +1,11 @@
+// Formulario para aportar saldo
+const formAporte = document.getElementById("formAporte");
+
+formAporte.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+});
+
+
+formAporte.addEventListener("submit", function(event) {
